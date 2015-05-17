5fb79455fa935b181977f46449491918fe4a2924
Golda Maier <golda.maier@hotmail.com>
2015-05-17 22:43:34 +0200

    added prompt

--- read.js
+++ read.js
@@ -1,11 +1,44 @@
 var http = require('http');
+var output = '';
 
+var prompt = require('prompt');
 
-http.get(options, function(res) {
-  console.log("Got response: " + res.statusCode);
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
+var properties = [
+  {
+    name: 'slug',
+    pattern: /([a-zA-Z0-9])/,
+    required: true
+  }
+];
+
+console.log('los');
+prompt.start();
+
+prompt.get(properties, function (err, result) {
+  if (err) { return onErr(err); }
+  console.log('  slug: ' + result.slug);
+
+  readPost(result.slug)
 });
+
+
+function onErr(err) {
+  console.log(err);
+  return 1;
+}
+
+
+//------------------------------------------------------
+
+function readPost(slug){
+
+  http.get(options, function(res) {
+    console.log("Got response: " + res.statusCode);
+
+
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
 
   var options = {
     host: 'localhost',
@@ -18,3 +51,5 @@ http.get(options, function(res) {
       // 'Content-Length': postData.length
     }
   };
+
+}
